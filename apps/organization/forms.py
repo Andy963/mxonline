1a06bd786b29bf345b4834591e8a0d3ff3496d72
Andy963 <zjgxs518@126.com>
2018-01-24 21:17:26 +0800

    check user's question input about course

--- apps/organization/forms.py
+++ apps/organization/forms.py
@@ -5,19 +5,20 @@
 
 from django import forms
 from operation.models import UserAsk
+import re
 
 # modelform的使用
 class UserAskForm(forms.ModelForm):
 
     class Meta:
         model = UserAsk
-        fields = ['name', 'mobile', 'course_name']
+        fields = ["name", "mobile", "course_name"]
 
     def clean_mobile(self):
         """
         验证手机号是否合法
         """
-        mobile = self.cleaned_data['mobile']
+        mobile = self.cleaned_data["mobile"]
         REGEX_MOBILE = "^1[358]\d{9}$|^147\d{8}|^176\d{8}$"
         p = re.compile(REGEX_MOBILE)
         if p.match(mobile):
--- apps/organization/views.py
+++ apps/organization/views.py
@@ -56,11 +56,13 @@ class OrgView(View):
                        })
 
 class AddUserAskView(View):
+    """
+    用户添加咨询
+    """
     def post(self, request):
         userask_form = UserAskForm(request.POST)
-        if userask_form.valid():
+        if userask_form.is_valid():
             user_ask = userask_form.save(commit=True)
-            return HttpResponse("{'status':'success'}", content_type="application/json")
-
+            return HttpResponse('{"status":"success"}', content_type="application/json")
         else:
-            return HttpResponse("{'status': 'fail', 'msg':{0}}".format(userask_form.errors),  content_type="application/json")
+            return HttpResponse('{"status": "fail", "msg":"格式出错"}',  content_type="application/json")
